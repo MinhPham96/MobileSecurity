16ad1c6fd1e47e7c044dd2a0f3377c0fcd628fe8
Minh Pham <s3535689@rmit.edu.vn>
2018-08-13 20:11:40 +0700

    Store device MAC address and get document ID

---
+++ app/src/main/java/com/example/minh/sensors/Device.java
@@ -0,0 +1,17 @@
+package com.example.minh.sensors;
+
+public class Device {
+    private String macAddress;
+
+    public Device(String macAddress) {
+        this.macAddress = macAddress;
+    }
+
+    public String getMacAddress() {
+        return macAddress;
+    }
+
+    public void setMacAddress(String macAddress) {
+        this.macAddress = macAddress;
+    }
+}
--- app/src/main/java/com/example/minh/sensors/MainActivity.java
+++ app/src/main/java/com/example/minh/sensors/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.minh.sensors;
 
 import android.content.Intent;
-import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -12,9 +10,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
@@ -23,12 +21,15 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.net.NetworkInterface;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+
+import javax.annotation.Nullable;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -36,15 +37,18 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "Main Activity";
     private static final String alertCollection = "alerts";
     private static final String historyCollection = "history" ;
+    private static final String deviceCollection = "devices" ;
+    private static final String macAddress = getMacAddr();
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 
-    private TextView newAlertTextView;
+    private Button onlineButton;
+    private TextView newAlertTextView, macTextView;
     private RecyclerView alertRecyclerView;
     private AlertAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
     private List<Alert> alertList = new ArrayList<>();
 
-    private FirebaseFirestore mFirebaseFirestore;
+    private FirebaseFirestore mFirestore;
     private DocumentReference alertDocRef;
     private boolean initCheck = false;
 
@@ -53,8 +57,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mFirebaseFirestore = FirebaseFirestore.getInstance();
-        alertDocRef = mFirebaseFirestore.collection(alertCollection).document("current_user");
+        mFirestore = FirebaseFirestore.getInstance();
+        alertDocRef = mFirestore.collection(alertCollection).document("current_user");
+
+        macTextView = (TextView) findViewById(R.id.macTextView);
+        macTextView.setText(macAddress);
+
+        onlineButton = (Button) findViewById(R.id.onlineButton);
+        onlineButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addNewDevice();
+            }
+        });
 
         newAlertTextView = (TextView) findViewById(R.id.newAlertTextView);
 
@@ -92,6 +107,19 @@ public class MainActivity extends AppCompatActivity {
         mAdapter = new AlertAdapter(alertList);
         alertRecyclerView.setAdapter(mAdapter);
 
+        //check if the Firestore has the current device, if yes, display the document ID
+        mFirestore.collection(deviceCollection).whereEqualTo("macAddress", macAddress)
+                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()) {
+                    for(DocumentSnapshot documentSnapshot : task.getResult().getDocuments()) {
+                        macTextView.setText(documentSnapshot.getId());
+                    }
+                }
+            }
+        });
+
     }
 
     @Override
@@ -99,7 +127,7 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         //get all the document in collection "history"
         //order them by the date
-        mFirebaseFirestore.collection(historyCollection).orderBy("date", Query.Direction.ASCENDING)
+        mFirestore.collection(historyCollection).orderBy("date", Query.Direction.ASCENDING)
                 .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
@@ -116,8 +144,56 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void addNewDevice() {
+        //check if the current device is stored in the Firestore or not
+        mFirestore.collection(deviceCollection).whereEqualTo("macAddress", macAddress)
+                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                //if not
+                if(task.getResult().isEmpty()) {
+                    //add new device to Firestore
+                    Device device = new Device(macAddress);
+                    mFirestore.collection(deviceCollection).add(device).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                        @Override
+                        public void onSuccess(DocumentReference documentReference) {
+                            Log.i(TAG, "save device");
+                        }
+                    });
+                }
+            }
+        });
+    }
+
     public void moveToSensor(View view) {
         Intent intent = new Intent(MainActivity.this, SensorActivity.class);
         startActivity(intent);
+    }
+
+    public static String getMacAddr() {
+        try {
+            List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface nif : all) {
+                if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
+
+                byte[] macBytes = nif.getHardwareAddress();
+                if (macBytes == null) {
+                    return "";
+                }
+
+                StringBuilder res1 = new StringBuilder();
+                for (byte b : macBytes) {
+                    res1.append(String.format("%02X:",b));
+                }
+
+                if (res1.length() > 0) {
+                    res1.deleteCharAt(res1.length() - 1);
+                }
+                return res1.toString();
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return "02:00:00:00:00:00";
     }
 }
